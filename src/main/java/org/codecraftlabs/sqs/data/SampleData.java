d0a75b1bb10c716466cc7d78cee7354b735858b5
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-25 22:45:35 +0100

    more code

---
+++ src/main/java/org/codecraftlabs/sqs/data/SampleData.java
@@ -0,0 +1,31 @@
+package org.codecraftlabs.sqs.data;
+
+public class SampleData {
+    private String id;
+    private String name;
+    private String programmingLanguage;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+}
