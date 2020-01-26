686c73a1738bbd9c55c37c059ca8e886a64e6f7a
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-26 16:56:53 +0100

    more code

--- src/main/java/org/codecraftlabs/sqs/service/AWSServiceExecutor.java
+++ src/main/java/org/codecraftlabs/sqs/service/AWSServiceExecutor.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.codecraftlabs.sqs.util.AppArguments;
 
 import static org.codecraftlabs.sqs.util.AppArguments.INTERVAL_SECONDS_OPTION;
-import static org.codecraftlabs.sqs.util.AppArguments.REGION_OPTION;
 import static org.codecraftlabs.sqs.util.AppArguments.SQS_URL_OPTION;
 
 public class AWSServiceExecutor {
@@ -13,7 +12,6 @@ public class AWSServiceExecutor {
 
     public void execute(AppArguments args) throws AWSException {
         var sqsUrl = args.option(SQS_URL_OPTION);
-        var awsRegion = args.option(REGION_OPTION);
         var interval = args.option(INTERVAL_SECONDS_OPTION);
     }
 }
